dd629756286171d2b77b38de2c8cda7e46a8f196
Equim <sayaka@ekyu.moe>
2017-09-01 14:22:47 +0800

    activate travis

--- doc.go
+++ doc.go
@@ -1,6 +1,4 @@
-/*
-	Package base91 implements a base91 encoder and decoder.
-
-	base91 is defined in Joachim Henke's basE91: http://base91.sourceforge.net/
-*/
+// Package base91 implements a base91 encoder and decoder.
+//
+// base91 is defined in Joachim Henke's basE91: http://base91.sourceforge.net/
 package base91 // import "ekyu.moe/base91"
